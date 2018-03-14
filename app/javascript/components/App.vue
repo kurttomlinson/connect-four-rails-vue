5ec7948abf37e91a3b624273d0960737a3845fcc
Kurt Tomlinson <kurttomlinson@gmail.com>
2018-03-13 21:29:07 -0500

    got vue working

---
+++ app/javascript/components/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    Hello from Vue!
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data () {
+    return {
+      moves: []
+    }
+  },
+  props: { gameId: null }
+}
+</script>
+
+<style scoped>
+</style>
